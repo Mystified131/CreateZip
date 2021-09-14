1f6e4b78c4b2dd0afd69c9a06f993413f15b4e96
Thomas Park <mystifiedthomas@gmail.com>
2021-09-14 04:06:40 -0500

    twaking

---
+++ CreateZip.py
@@ -0,0 +1,16 @@
+
+import shutil
+
+zipdir = "C:\\Users\\mysti\\Desktop\\Instax2"
+
+print("")
+
+print("Zipping Files.")
+
+print("")
+
+shutil.make_archive('ZipArch', 'zip', zipdir)
+
+print("Files zipped and ready.")
+
+print("")
